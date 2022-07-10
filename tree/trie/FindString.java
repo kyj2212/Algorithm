f697c74294c60fa6872be1f668accdc2d5ee2ec9
kyj2212 <kyj2212@gmail.com>
2022-07-10 22:33:46 +0900

    trie sol 01 -- 정답률 60%

--- tree/trie/FindString.java
+++ tree/trie/FindString.java
@@ -5,8 +5,8 @@ import java.util.StringTokenizer;
 
 public class FindString {
 
-    static int N;
-    static int M;
+    static int N; // 키 문자열의 개수.
+    static int M; // 검사해야하는 문자열의 개수
 
     public static void main(String[] args) throws IOException {
 
@@ -21,56 +21,84 @@ public class FindString {
 
 
         String[] S = new String[N];
-        String[] test = new String[M];
-
+        String test;
+        int cnt=0;
 
         for (int i = 0; i < N; i++) {
             S[i]=br.readLine();
         }
 
+        TrieNode trie = InsertToTrie(S);
+
+
         for (int i = 0; i < M; i++) {
-            test[i]=br.readLine();
+            test=br.readLine();
+            if(SearchString(trie,test))
+                cnt++;
         }
 
 
         br.close();
 
-        for(String s : S)
-            System.out.printf(s+ " ");
-        System.out.println();
-
-        for(String s : test)
-            System.out.printf(s+ " ");
-        System.out.println();
 
 
+        System.out.println(cnt);
 
 
-        //System.out.println(SearchString(S,test));
 
     }
 
-    static int SearchString(String[] S, String[] test){
+    static TrieNode InsertToTrie(String[] S){
 
-        Node root = new Node();
+        // root 노드 생성
+        TrieNode root = new TrieNode();
+
+        // 대상 key 문자열을 trie에 insert
         for(int n =0;n<N;n++){
-            for(char c : S[n].toCharArray()){
-                if(root.next[c-'a']==null)
-                    root.next[c-'a']=new Node();
+            TrieNode node = root;
+            // 각각의 문자열 S[n] 에 대하여
+            for(int i=0;i<S[n].length();i++){
+                char c = S[n].charAt(i);
+                // S[n]의 charAt[] 에 대하여 (문자열의 철자 1개씩)
+                if(node.child[c-'a']==null) { // child가 없으면
+                    node.child[c-'a']=new TrieNode(); // child 추가
+
+                    // 문자열의 마지막 char 일때
+                    if(i==S[n].length()-1) {
+                        node.ifLeaf=true;
+                        break; // 자식노드로 내려가지말고 해당 string 종료
+                    }
+                }
+                node=node.child[c-'a']; // 자식 노드로 내려가.
 
             }
         }
 
-        return 0;
+
+        return root;
 
     }
 
+    static boolean SearchString(TrieNode trie,String test){
+        // test 문자열이 trie에 해당하는지 search
 
+        for (int i=0;i<test.length();i++){
+            char c = test.charAt(i);
+            if(trie.child[c-'a']==null) // trie 없으면 꽝
+                return false;
+            else {
+                trie=trie.child[c-'a'];
+                if(trie.ifLeaf==true)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    static class TrieNode{
+        TrieNode[] child = new TrieNode[26];
+        Boolean ifLeaf=false;
+    }
 
 }
 
-
-class Node{
-    Node[] next = new Node[26];
-    Boolean ifLeaf;
-}
