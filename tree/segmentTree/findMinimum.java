02e5e12230a8094810abbef3ae6d5427485aa6ae
kyj2212 <kyj2212@gmail.com>
2022-07-16 23:27:28 +0900

    use node class but take long than 1 array

--- tree/segmentTree/findMinimum.java
+++ tree/segmentTree/findMinimum.java
@@ -5,15 +5,35 @@ import java.util.StringTokenizer;
 
 /*
 * range minimum qeury with segment tree
-* use 1 degree array
-* 59168kb	668ms
+* use Node class
+* 65976	696 / 65512	700
 * */
 
 public class findMinimum {
 
     static int N,M;
     static int [] input;
-    static int [] seg;
+
+    static class Node {
+        int start;
+        int end;
+        int min;
+        Node left;
+        Node right;
+
+        Node(int start, int end){
+            this.start=start;
+            this.end=end;
+            int half = start+(end-start)/2;
+            if(start<=half&& half+1<=end){
+                this.left=new Node(start,half);
+                this.right=new Node(half+1,end);
+            }
+        }
+        int getMin(){return this.min;}
+    }
+
+
 
     public static void main(String[] args) throws IOException {
 
@@ -28,18 +48,14 @@ public class findMinimum {
         for(int i=0;i<N;i++){
             input[i]=Integer.parseInt(br.readLine());
         }
-        buildSegmentTree(N);
-
-/*        for(int i: seg){
-            System.out.printf(i+" ");
-        }*/
+       Node root = buildSegmentTree();
 
         for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine()," ");
             int qs=Integer.parseInt(st.nextToken());
             int qe=Integer.parseInt(st.nextToken());
 
-            bw.write(String.valueOf(RMQ(0,N-1,0,qs-1,qe-1))+'\n');
+            bw.write(String.valueOf(RMQ(root,qs-1,qe-1))+'\n');
         }
 
         br.close();
@@ -48,55 +64,39 @@ public class findMinimum {
     }
 
 
-    static int RMQ(int ss, int se, int nodeidx,int qs, int qe) {
+    static int RMQ(Node node, int qs, int qe) {
 
-        if(ss>qe||se<qs){ // qs,qe 2,4  ss,se 5,9 걸러
+        if(node.start>qe||node.end<qs){ // qs,qe 2,4  ss,se 5,9 걸러
             return 1000000001;
-        }else if(qs<=ss&&se<=qe){ //qs,qe 보다 범위가 작아, 큰범위부터 트리가 내려가기때문에 작은값은 무조건 min에서 비교를 간다.
-            return seg[nodeidx];
+        }else if(qs<=node.start&&node.end<=qe){ //qs,qe 보다 범위가 작아, 큰범위부터 트리가 내려가기때문에 작은값은 무조건 min에서 비교를 간다.
+            return node.getMin();
         }else{
-            int leftidx=2*nodeidx+1;
-            int half=ss+(se-ss)/2;
-            if(leftidx<seg.length) {
-                return Math.min(RMQ(ss, half, leftidx, qs, qe), // 0,4 --> 0~2, 3~4 로 쪼개져 3~4  --> 0~2는 0~1(걸러), 2(return 100) --> 3~4 범위안이라서 return 38 --> 100이랑 38 비교해 38,
-                        RMQ(half + 1, se, leftidx + 1, qs, qe)); //5,9 --> 걸러져 // 38이랑 거른거 비교해 38
+            if(node.left!=null) { // left가 없고 right 만 있을 수는 없어
+                return Math.min(RMQ(node.left,qs, qe), // 0,4 --> 0~2, 3~4 로 쪼개져 3~4  --> 0~2는 0~1(걸러), 2(return 100) --> 3~4 범위안이라서 return 38 --> 100이랑 38 비교해 38,
+                        RMQ(node.right, qs, qe)); //5,9 --> 걸러져 // 38이랑 거른거 비교해 38
             }
         }
         return 1000000001;
     }
 
-    static void buildSegmentTree(int N) {
-        double k= (Math.log(N)/Math.log(2));
-        int height= k>(int)k?(int)k+1:(int)k;
-
-        seg = new int[(int)Math.pow(2,height+1)-1];
-
-        for(int i=0;i<seg.length;i++)
-            seg[i]=1000000001; // 큰값으로 초기화
-//        System.out.println("node개수 "+((int)Math.pow(2,height+1)-1));
-        seg(0,N-1,0);
-
+    static Node buildSegmentTree() {
+        Node root = new Node(0, N-1);
+        seg(root);
+        return root;
     }
 
-    static int seg(int start,int end,int nodeidx){
 
 
-        if(start==end){
-            seg[nodeidx]=input[start];
-            return seg[nodeidx];
+    static int seg(Node node){
+
+        if(node.start==node.end){
+            node.min=input[node.start];
+            return node.getMin();
         }else{
-
-            int half=start+(end-start)/2;
-            int leftidx=nodeidx*2+1;
-
-            seg[nodeidx]=Math.min(seg(start,half,leftidx)
-                    ,seg(half+1,end,leftidx+1));
-            return seg[nodeidx];
-
+            node.min=Math.min(seg(node.left)
+                    ,seg(node.right));
+            return node.getMin();
         }
-
-
     }
-
 
 }
