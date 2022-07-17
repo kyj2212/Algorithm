6411ee14642ba9347f27ad5f1e18783178521435
kyj2212 <kyj2212@gmail.com>
2022-07-17 23:22:37 +0900

    use adj matrix but same time

--- search/DFSvsBFS.java
+++ search/DFSvsBFS.java
@@ -7,7 +7,8 @@ public class DFSvsBFS {
 
 
     static int N,M,V;
-    static ArrayList<Integer> [] adj;
+    //static ArrayList<Integer> [] adj;
+    static boolean [][] adj;
     static boolean [] dfsVisited;
 
     public static void main(String[] args) throws IOException {
@@ -22,12 +23,15 @@ public class DFSvsBFS {
         V=Integer.parseInt(st.nextToken());
 
 
-        adj = new ArrayList[N+1];
+        adj=new boolean[N+1][N+1];
 
+
+        // adj list
+        /*
+
+        adj = new ArrayList[N+1];
         for(int i =0;i<N+1;i++)
             adj[i]=new ArrayList<>();
-
-
 
         // 그래프를 - 인접리스트로 표현
         // node에 대한 인접리스트
@@ -46,7 +50,22 @@ public class DFSvsBFS {
             Collections.sort(adj[i]);
            // adj[i].sort(Comparator.comparingInt(a -> a));
 
+*/
 
+
+        // 그래프를 - 인접 행렬로 표현
+        for(int i=0;i<M;i++){// edge에 이어져 있다면, 그것은 인접한 노드니까 인접리스트에 넣기
+            st = new StringTokenizer(br.readLine()," ");
+            int node1= Integer.parseInt(st.nextToken());
+            int node2= Integer.parseInt(st.nextToken());
+            // 양방향(무방향)
+            adj[node1][node2]=true;
+            adj[node2][node1]=true;
+        }
+
+        // 정렬할 필요가 없지. 이미 순서대로 잇어.
+
+/*
         for(ArrayList<Integer> list : adj){
             if(list.isEmpty())
                 continue;
@@ -54,7 +73,7 @@ public class DFSvsBFS {
                 System.out.printf(i+" ");
             System.out.println();
         }
-
+*/
 
         // 그래프 탐색
 
@@ -68,29 +87,27 @@ public class DFSvsBFS {
 
     }
 
-
-
     // DFS 로 탐색
+    // with 인접 행렬
     private static String dfs(int node) {
         dfsVisited[node]=true;
         // 노드의 child로 가는거니까.
         String answer=String.valueOf(node);
-        for(int i : adj[node]){
-            if(!dfsVisited[i])
-                answer+=" "+dfs(i);
+        for(int i=1;i<N+1;i++){
+            if(adj[node][i]){
+                if(!dfsVisited[i])
+                    answer+=" "+dfs(i);
+            }
         }
         return answer;
     }
 
-
-    // BFS로 탐색
+    // BFS
+    // with 인접 행렬
     static String bfs(int start){
-        // queue 에 넣는다.
 
         String answer="";
         Queue<Integer> queue = new LinkedList<>();
-     //  Queue<Integer> pq = new PriorityQueue<>(); // 이전에 넣은 queue가 먼저 실행되버려. 안돼
-                                                    // 작은값을 순서대로 하려면 adj를 정렬해야한다.
 
         boolean bfsVisited[]=new boolean[N+1];
 
@@ -101,14 +118,67 @@ public class DFSvsBFS {
             if(node!=start)
                 answer+=" ";
             answer += String.valueOf(node);
-            for(int i : adj[node])
-                if(!queue.contains(i)&&!bfsVisited[i])
-                    queue.add(i);
+            for(int i=1;i<N+1;i++){
+                if(adj[node][i]){
+                    if(!queue.contains(i)&&!bfsVisited[i])
+                        queue.add(i);
+                }
+            }
         }
 
         return answer;
 
     }
+
+
+
+
+    // DFS 로 탐색
+    // with 인접 리스트
+    /*
+
+    private static String dfs(int node) {
+        dfsVisited[node]=true;
+        // 노드의 child로 가는거니까.
+        String answer=String.valueOf(node);
+        for(int i : adj[node]){
+            if(!dfsVisited[i])
+                answer+=" "+dfs(i);
+        }
+        return answer;
+    }
+*/
+
+
+    // BFS로 탐색
+    // with 인접 리스트
+    /*
+        static String bfs(int start){
+            // queue 에 넣는다.
+
+            String answer="";
+            Queue<Integer> queue = new LinkedList<>();
+         //  Queue<Integer> pq = new PriorityQueue<>(); // 이전에 넣은 queue가 먼저 실행되버려. 안돼
+                                                        // 작은값을 순서대로 하려면 adj를 정렬해야한다.
+
+            boolean bfsVisited[]=new boolean[N+1];
+
+            queue.add(start); // 초기화
+            while(!queue.isEmpty()) {
+                int node = queue.poll();
+                bfsVisited[node]=true;
+                if(node!=start)
+                    answer+=" ";
+                answer += String.valueOf(node);
+                for(int i : adj[node])
+                    if(!queue.contains(i)&&!bfsVisited[i])
+                        queue.add(i);
+            }
+
+            return answer;
+
+        }
+    */
 
 
 
