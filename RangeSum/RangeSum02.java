1a4004407f4e3545adf1f8838466f74662e94e5f
kyj2212 <kyj2212@gmail.com>
2022-07-18 23:34:35 +0900

    FIX: back ?: 800ms

--- RangeSum/RangeSum02.java
+++ RangeSum/RangeSum02.java
@@ -22,12 +22,8 @@ public class RangeSum02 {
 
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine()," ");
-            prefixSum[i][0]=(i<1?0:prefixSum[i-1][0])+Integer.parseInt(st.nextToken());
-            for (int j = 1; j < N; j++) {
-                if(i==0)
-                    prefixSum[0][j]=prefixSum[0][j-1]+Integer.parseInt(st.nextToken());
-                //int next =Integer.parseInt(st.nextToken());
-               else prefixSum[i][j]=prefixSum[i][j-1]+prefixSum[i-1][j]-prefixSum[i-1][j-1]+Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                prefixSum[i][j]=(j<1?0:prefixSum[i][j-1])+(i<1?0:prefixSum[i-1][j])-((i<1||j<1)?0:prefixSum[i-1][j-1])+Integer.parseInt(st.nextToken());
             }
         }
 /*
@@ -44,17 +40,8 @@ public class RangeSum02 {
             int qsy=Integer.parseInt(st.nextToken())-1;
             int qex=Integer.parseInt(st.nextToken())-1;
             int qey=Integer.parseInt(st.nextToken())-1;
-            // s(qex,qey) - s(qsx-1,qey) - s(qex,qsy-1) + s(qsx-1,qsy-1)
-            if(qsx<1||qsy<1){
-                if(qsx>=1)
-                    sb.append(prefixSum[qex][qey]-prefixSum[qsx-1][qey]).append("\n");
-                else if(qsy>=1)
-                    sb.append(prefixSum[qex][qey]-prefixSum[qex][qsy-1]).append("\n");
-                else
-                    sb.append(prefixSum[qex][qey]).append("\n");
-            } else
-                sb.append(prefixSum[qex][qey] -prefixSum[qsx-1][qey] -prefixSum[qex][qsy-1] +prefixSum[qsx-1][qsy-1]).append("\n");
-
+            int sum=prefixSum[qex][qey] - (qsx<1?0:prefixSum[qsx-1][qey]) -(qsy<1?0:prefixSum[qex][qsy-1]) +((qsx<1||qsy<1)?0:prefixSum[qsx-1][qsy-1]);
+            sb.append(sum).append("\n");
         }
 
         br.close();
