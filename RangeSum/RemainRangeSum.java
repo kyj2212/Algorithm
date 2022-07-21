a56a7f4aa28865867f87bd9f18da6847eb78fbcf
kyj2212 <kyj2212@gmail.com>
2022-07-21 21:22:42 +0900

    FEAT: prefix sum, but time over

--- RangeSum/RemainRangeSum.java
+++ RangeSum/RemainRangeSum.java
@@ -1,2 +1,36 @@
-package RangeSum;public class RemainRangeSum {
+package RangeSum;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class RemainRangeSum {
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+
+        int [] prefixSum = new int[N];
+        st = new StringTokenizer(br.readLine()," ");
+        prefixSum[0]=Integer.parseInt(st.nextToken())%M;
+        for (int i = 0; i < N-1; i++) {
+            prefixSum[i+1]=(Integer.parseInt(st.nextToken()))%M+(prefixSum[i])%M;
+        }
+
+
+        int cnt=0;
+
+
+
+        bw.write(String.valueOf(cnt));
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
