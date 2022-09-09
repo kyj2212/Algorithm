1210512eae0419141241fd7f775cc986d6c445e0
kyj2212 <kyj2212@gmail.com>
2022-09-09 14:45:17 +0900

    Fix: Biginteger() time over

--- hashing/PhoneNumberList.java
+++ hashing/PhoneNumberList.java
@@ -1,11 +1,12 @@
 package hashing;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneNumberList {
     public static void main(String[] args) {
-        String[] phone_book = {"12","123","1235","567","88"};
+        String[] phone_book = {"12","1122223","1235666666666666666666666","567","88"};
         boolean answer = true;
 
         Map<String,Boolean> map = new HashMap<>();
@@ -19,23 +20,17 @@ public class PhoneNumberList {
             }
             map.put(phone,true);
         }
-
         System.out.println(answer);
     }
     static boolean findSub(String a, String b){
-        String key,target;
+        BigInteger key,target;
         if(a.length()> b.length()){
-            key = b;
-            target = a;
+            key = new BigInteger(b);
+            target = new BigInteger(a.substring(0,b.length()));
         }else{
-            target=b;
-            key = a;
+            key = new BigInteger(a);
+            target= new BigInteger(b.substring(0,a.length()));
         }
-        for(int i =0;i<key.length();i++){
-            if(target.charAt(i)!=key.charAt(i)){
-                return true;
-            }
-        }
-        return false;
+        return key.equals(target) ? false : true;
     }
 }
