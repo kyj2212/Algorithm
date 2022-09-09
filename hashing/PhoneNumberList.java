18389f8d19ef3beda13e741346732134505f187d
kyj2212 <kyj2212@gmail.com>
2022-09-09 14:15:34 +0900

    Fix: findsub charAt, time over

--- hashing/PhoneNumberList.java
+++ hashing/PhoneNumberList.java
@@ -1,29 +1,41 @@
 package hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneNumberList {
     public static void main(String[] args) {
-        String[] phone_book = {"119", "97674223", "1195524421"};
+        String[] phone_book = {"12","123","1235","567","88"};
         boolean answer = true;
 
-
-        Phone number = new Phone();
-        L1 :
+        Map<String,Boolean> map = new HashMap<>();
+        L1:
         for(String phone : phone_book){
-            for(int i=0;i<phone.length();i++){
-
-                if(number.child[phone.charAt(i)-'0']==null){
-                    number.child[phone.charAt(i)-'0']=new Phone();
-                    continue;
-                }else if(i==phone.length()-1){
+            for(String key : map.keySet()){
+                if(!findSub(key,phone)){
                     answer=false;
                     break L1;
                 }
             }
+            map.put(phone,true);
         }
 
         System.out.println(answer);
     }
-    static class Phone{
-        Phone[] child = new Phone[10];
+    static boolean findSub(String a, String b){
+        String key,target;
+        if(a.length()> b.length()){
+            key = b;
+            target = a;
+        }else{
+            target=b;
+            key = a;
+        }
+        for(int i =0;i<key.length();i++){
+            if(target.charAt(i)!=key.charAt(i)){
+                return true;
+            }
+        }
+        return false;
     }
 }
