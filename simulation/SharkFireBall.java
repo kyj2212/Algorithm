cd6c2d38dc76546bc0208aa79841b5f3539edca9
kyj2212 <kyj2212@gmail.com>
2022-09-15 00:31:05 +0900

    Fix: no int[] pos, but time same 800ms

--- simulation/SharkFireBall.java
+++ simulation/SharkFireBall.java
@@ -19,22 +19,14 @@ public class SharkFireBall {
         List<FireBall> fireBalls = new ArrayList<>();
         for(int i =0;i<M;i++){
             st = new StringTokenizer(br.readLine()," ");
-            int [] pos = new int[2];
-            for(int j=0;j<2;j++){
-                pos[j]=Integer.parseInt(st.nextToken());
-            }
-            fireBalls.add(new FireBall(pos,Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
+            fireBalls.add(new FireBall(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
         }
 
         for(int i=0;i<K;i++){
             for(FireBall fireBall : fireBalls){
                 move(fireBall);
             }
-            System.out.println((i+1)+"번째 after move");
-            print(fireBalls);
             fireBalls=merge(fireBalls);
-            System.out.println("after merge");
-            print(fireBalls);
         }
         int sum=0;
         for(FireBall fireBall : fireBalls){
@@ -42,11 +34,6 @@ public class SharkFireBall {
         }
 
         System.out.println(sum);
-    }
-    static void print(List<FireBall> fireBalls){
-        for(FireBall fireBall : fireBalls) {
-            System.out.println(fireBall.pos[0]+","+fireBall.pos[1] + ", " + fireBall.m + "," + fireBall.s + "," + fireBall.d);
-        }
     }
     static List<FireBall> merge(List<FireBall> fireBalls){
         List<FireBall> fireBallList = new ArrayList<>();
@@ -57,8 +44,8 @@ public class SharkFireBall {
         int [] idx = new int[N*N];
         int [] direction = new int[N*N]; // 1 모두 짝, 2 모두 홀, 3 다를때
         for(FireBall fireBall : fireBalls){
-            int r = fireBall.pos[0]-1;
-            int c = fireBall.pos[1]-1;
+            int r = fireBall.r-1;
+            int c = fireBall.c-1;
             cnt[r*N+c]++;
             idx[r*N+c]=fireBalls.indexOf(fireBall);
             sumM[r*N+c]+=fireBall.m;
@@ -77,13 +64,11 @@ public class SharkFireBall {
             if(cnt[i]>1 && sumM[i]/5>0){
                 if(direction[i]!=3){ // 모두 홀, 또는 짝
                     for(int j=0;j<=6;j+=2){
-                        int [] pos = {i/N+1,i%N+1};
-                        fireBallList.add(new FireBall(pos,sumM[i]/5,sumS[i]/cnt[i],j));
+                        fireBallList.add(new FireBall(i/N+1,i%N+1,sumM[i]/5,sumS[i]/cnt[i],j));
                     }
                 }else{ // 아닐 때
                     for(int j=1;j<=7;j+=2){
-                        int [] pos = {i/N+1,i%N+1};
-                        fireBallList.add(new FireBall(pos,sumM[i]/5,sumS[i]/cnt[i],j));
+                        fireBallList.add(new FireBall(i/N+1,i%N+1,sumM[i]/5,sumS[i]/cnt[i],j));
                     }
                 }
             }
@@ -92,16 +77,16 @@ public class SharkFireBall {
     }
     static FireBall move(FireBall f){
         if(f.d==7 || f.d==0 || f.d==1){
-            f.pos[0]=left(f.pos[0],f.s%N);
+            f.r=left(f.r,f.s%N);
         }
         if(f.d==5||f.d==4||f.d==3){
-            f.pos[0]=right(f.pos[0],f.s%N);
+            f.r=right(f.r,f.s%N);
         }
         if(f.d==7||f.d==6||f.d==5){
-            f.pos[1]=up(f.pos[1],f.s%N);
+            f.c=up(f.c,f.s%N);
         }
         if(f.d==1||f.d==2||f.d==3){
-            f.pos[1]=down(f.pos[1],f.s%N);
+            f.c=down(f.c,f.s%N);
         }
         return f;
     }
@@ -118,17 +103,24 @@ public class SharkFireBall {
         return c+s>N ? c+s-N : c+s;
     }
     static class FireBall {
-        int[] pos = new int[2];
+        int r;
+        int c;
         int m;
         int s;
         int d;
 
-        FireBall(int[] pos, int m, int s, int d){
-            this.pos=pos;
+        FireBall(int r, int c,  int m, int s, int d){
+            this.r=r;
+            this.c=c;
             this.m=m;
             this.s=s;
             this.d=d;
         }
 
+    }
+    static void print(List<FireBall> fireBalls){
+        for(FireBall fireBall : fireBalls) {
+            System.out.println(fireBall.r+","+fireBall.c + ", " + fireBall.m + "," + fireBall.s + "," + fireBall.d);
+        }
     }
 }
