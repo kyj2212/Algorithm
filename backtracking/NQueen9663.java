d83468b0ef910b6030c535fa023b17a523593879
kyj2212 <kyj2212@gmail.com>
2022-10-09 15:50:17 +0900

    Fix: isvalid <= x but time over

--- backtracking/NQueen9663.java
+++ backtracking/NQueen9663.java
@@ -20,24 +20,30 @@ public class NQueen9663 {
             queen[r]=-1;
         }
 
-        dfs(0,queen,new boolean[N][N]);
+        dfs(0,queen);
         System.out.println(cnt);
     }
-    static int[] dfs(int r,int[] queen, boolean[][] inValid){
+    static int[] dfs(int r,int[] queen){
         if(r==N){
-/*            System.out.println(" 현재 row : "+r + " queen : ");
+            System.out.println(" 현재 row : "+r + " queen : ");
             for(int i=0;i<N;i++){
                 System.out.printf("( "+i+","+queen[i]+" ) ");
             }
-            System.out.println();*/
+            System.out.println();
             cnt++;
             return queen;
         }
-        // int[] queen = new int[N];
 
-        for(int c=0;c<N;c++){
+        for(int c=0;c<N;c++) {
 
-            if(!inValid[r][c]){
+            if (isValid(r, c, queen)) {
+                queen[r]=c;
+                queen=dfs(r+1,queen);
+            }
+            queen[r]=-1;
+
+        }
+/*            if(!inValid[r][c]){
                 queen[r]=c;
                 boolean[][] backValid = back(inValid);
                 inValid=checkValid(r,c,inValid);
@@ -45,23 +51,13 @@ public class NQueen9663 {
                 inValid=backValid;
             }
             queen[r]=-1;
-        }
+        }*/
         return queen;
     }
 
-    private static boolean[][] back(boolean[][] inValid){
-        boolean[][] back=new boolean[N][N];
-        for(int i =0;i<N;i++){
-            for(int j =0;j<N;j++){
-                back[i][j]=inValid[i][j];
-            }
-        }
-        return back;
-    }
-/*
     static boolean isValid(int nr,int nc,int[] queen){
         int r,c;
-        for(int i=0;i<N;i++){
+        for(int i=0;i<=nr;i++){
            if(queen[i]==-1){
                break;
            }
@@ -70,7 +66,7 @@ public class NQueen9663 {
             if(r==nr| c==nc){
                 return false;
             }
-           for(int j=1;j<N;j++){
+           for(int j=1;j<=nr-r;j++){
                if(r+j==nr && c+j == nc){
                    return false;
                }
@@ -86,32 +82,6 @@ public class NQueen9663 {
            }
         }
         return true;
-    }
-*/
-
-    // N*N 의 체스판에서 queen 서로 공격하지 못하게 놓는다.
-    // queen 은 모든 방향으로 움직일 수 있다. x +- y +- x+y- x-y+ 4방향 이동거리 d N까지 가능
-    private static boolean[][] checkValid (int x, int y, boolean[][] inValid) {
-
-        for(int i =0;i<N;i++){
-            inValid[x][i]=true;
-            inValid[i][y]=true;
-        }
-        for(int i=1;i<N;i++){
-            if(x+i<N && y+i<N){
-                inValid[x+i][y+i]=true;
-            }
-            if(x+i<N && y-i>=0){
-                inValid[x+i][y-i]=true;
-            }
-            if(x-i>=0 && y-i>=0){
-                inValid[x-i][y-i]=true;
-            }
-            if(x-i>=0 && y+i<N){
-                inValid[x-i][y+i]=true;
-            }
-        }
-        return inValid;
     }
 
 }
@@ -140,3 +110,39 @@ public class NQueen9663 {
     }
 
 */
+// N*N 의 체스판에서 queen 서로 공격하지 못하게 놓는다.
+// queen 은 모든 방향으로 움직일 수 있다. x +- y +- x+y- x-y+ 4방향 이동거리 d N까지 가능
+/*    private static boolean[][] checkValid (int x, int y, boolean[][] inValid) {
+
+        for(int i =0;i<N;i++){
+            inValid[x][i]=true;
+            inValid[i][y]=true;
+        }
+        for(int i=1;i<N;i++){
+            if(x+i<N && y+i<N){
+                inValid[x+i][y+i]=true;
+            }
+            if(x+i<N && y-i>=0){
+                inValid[x+i][y-i]=true;
+            }
+            if(x-i>=0 && y-i>=0){
+                inValid[x-i][y-i]=true;
+            }
+            if(x-i>=0 && y+i<N){
+                inValid[x-i][y+i]=true;
+            }
+        }
+        return inValid;
+    }*/
+
+
+/*
+    private static boolean[][] back(boolean[][] inValid){
+        boolean[][] back=new boolean[N][N];
+        for(int i =0;i<N;i++){
+            for(int j =0;j<N;j++){
+                back[i][j]=inValid[i][j];
+            }
+        }
+        return back;
+    }*/
