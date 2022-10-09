9b3011ebefd8daa193d5f6ee9f59eb42067399ac
kyj2212 <kyj2212@gmail.com>
2022-10-09 15:22:42 +0900

    Fix: boolean[][] visited mem over

--- backtracking/NQueen9663.java
+++ backtracking/NQueen9663.java
@@ -20,10 +20,10 @@ public class NQueen9663 {
             queen[r]=-1;
         }
 
-        dfs(0,queen);
+        dfs(0,queen,new boolean[N][N]);
         System.out.println(cnt);
     }
-    static int[] dfs(int r,int[] queen){
+    static int[] dfs(int r,int[] queen, boolean[][] inValid){
         if(r==N){
 /*            System.out.println(" 현재 row : "+r + " queen : ");
             for(int i=0;i<N;i++){
@@ -34,17 +34,31 @@ public class NQueen9663 {
             return queen;
         }
         // int[] queen = new int[N];
+
         for(int c=0;c<N;c++){
 
-            if(isValid(r,c,queen)){
+            if(!inValid[r][c]){
                 queen[r]=c;
-                queen=dfs(r+1,queen);
+                boolean[][] backValid = back(inValid);
+                inValid=checkValid(r,c,inValid);
+                queen=dfs(r+1,queen,inValid);
+                inValid=backValid;
             }
             queen[r]=-1;
-
         }
         return queen;
     }
+
+    private static boolean[][] back(boolean[][] inValid){
+        boolean[][] back=new boolean[N][N];
+        for(int i =0;i<N;i++){
+            for(int j =0;j<N;j++){
+                back[i][j]=inValid[i][j];
+            }
+        }
+        return back;
+    }
+/*
     static boolean isValid(int nr,int nc,int[] queen){
         int r,c;
         for(int i=0;i<N;i++){
@@ -73,56 +87,34 @@ public class NQueen9663 {
         }
         return true;
     }
-/*
-    static int[][] dfs(int x,int[][] queens,int[][] before){
-        if(x==N){
-            System.out.println("cnt : "+cnt);
-            System.out.println("queen : ");
-            for(int i=0;i<N;i++){
-                for(int j=0;j<N;j++){
-                    if(queens[i][j]==1){
-                        System.out.printf("("+i+","+j+")");
-                    }
-                }
-            }
-            for(int[] queen : queens){
-                System.out.printf("("+queen[0]+","+queen[1]+")");
-            }
-            System.out.println();
-            cnt++;
-            return queens;
-        }
-        // queen 이 놓이면 1
-        // visited -1 (queen 못놓으면)
-        // 가능하면 0 (초기값)
-        for(int i =0;i<N;i++){
-
-            if(queens[x][i]==0){
-                int[][] tmp = queens;
-                queens=checkValid(x,i,queens);
-                queens[x][i]=1;
-                queens=dfs(x+1,queens,before);
-            }
-            System.out.println(String.format("%d 번째 queen : ", x));
-            for(int r=0;r<N;r++){
-                for(int j=0;j<N;j++){
-                    if(queens[r][j]==1){
-                        System.out.printf("("+r+","+j+")");
-                    }
-                }
-            }
-            System.out.println();
-           // x 번째로 back 했으니까 이전 valid 돌려야해
-           queens=before;
-
-        }
-        return queens;
-    }
 */
-
 
     // N*N 의 체스판에서 queen 서로 공격하지 못하게 놓는다.
     // queen 은 모든 방향으로 움직일 수 있다. x +- y +- x+y- x-y+ 4방향 이동거리 d N까지 가능
+    private static boolean[][] checkValid (int x, int y, boolean[][] inValid) {
+
+        for(int i =0;i<N;i++){
+            inValid[x][i]=true;
+            inValid[i][y]=true;
+        }
+        for(int i=1;i<N;i++){
+            if(x+i<N && y+i<N){
+                inValid[x+i][y+i]=true;
+            }
+            if(x+i<N && y-i>=0){
+                inValid[x+i][y-i]=true;
+            }
+            if(x-i>=0 && y-i>=0){
+                inValid[x-i][y-i]=true;
+            }
+            if(x-i>=0 && y+i<N){
+                inValid[x-i][y+i]=true;
+            }
+        }
+        return inValid;
+    }
+
+}
 /*
     private static int[][] checkValid (int x, int y, int[][] queens) {
 
@@ -148,5 +140,3 @@ public class NQueen9663 {
     }
 
 */
-
-}
