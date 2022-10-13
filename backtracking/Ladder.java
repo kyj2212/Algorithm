99bb197a4b97478d5ef60bee1623dd469fa26919
kyj2212 <kyj2212@gmail.com>
2022-10-13 14:27:29 +0900

    Chore: alige comment

--- backtracking/Ladder.java
+++ backtracking/Ladder.java
@@ -26,12 +26,12 @@ public class Ladder{
             if(b+1<=N)
                 ladder[b+1][a]=-1; // b번 - b+1번 세로줄 사이의 a 번째 가로줄이 존재
         }
-//        printLadder(ladder);
+
         List<int[]> nodes = new ArrayList<>();
         List<Integer> cnt = new ArrayList<>();
 
         if(!isSame(ladder)){
-            cnt = dfs(ladder,0,nodes,cnt);
+            cnt = dfs(ladder,nodes,cnt);
         }else{
             cnt.add(0);
         }
@@ -40,47 +40,29 @@ public class Ladder{
     }
 
 
-    private static List<Integer> dfs(int[][] ladder,int depth,List<int[]> nodes,List<Integer> cnt){
-
+    private static List<Integer> dfs(int[][] ladder, List<int[]> nodes,List<Integer> cnt){
         if(nodes.size()>4){
             return cnt;
         }
-
         for(int v=1;v<=N;v++){
             for(int h=1;h<=H;h++){
-
                 int[][] tmp = addLine(ladder,v,h);
-                int [] node = new int[]{v,h};
                 if(tmp==null){
                     continue;
                 }
+                int [] node = new int[]{v,h};
                 ladder=tmp;
-//                cnt++;
                 nodes.add(node);
-//                printNode(nodes);
-//                System.out.println(String.format("cnt: %d, (%d,%d) 추가",cnt, v,h));
-//                System.out.println(String.format("ladder size: %d, cnt: %d", count(ladder)-M,cnt));
                 if(isSame(ladder)){
-//                    printLadder(ladder);
                     cnt.add(nodes.size());
                 }
                 if(nodes.size()<3){
-//                    List<Integer> result = dfs(ladder,depth,nodes,cnt);
-                    cnt = dfs(ladder,depth,nodes,cnt);
-//                    System.out.println(String.format("depth: %d, cnt: %d", depth,cnt));
-//                    if(result!=null){
-////                        System.out.println(String.format("depth: %d가 -1이 아닌경우", depth));
-//                        return result;
-//                    }
-
+                    cnt = dfs(ladder,nodes,cnt);
                 }
                 ladder=delLine(ladder,v,h);
                 nodes.remove(node);
-//                System.out.println(String.format("cnt: %d, (%d,%d) 제거", cnt,v,h));
-//                System.out.println(String.format("ladder size: %d, cnt: %d", count(ladder)-M,cnt));
             }
         }
-//        System.out.println(String.format("cnt : %d,다돌았는데 no same, cnt>=3, cnt -1 반환 ", cnt));
         return cnt;
     }
 
@@ -93,7 +75,6 @@ public class Ladder{
     private static boolean isSame(int[][] ladder){
         for(int v=1;v<=N;v++){
             if(!down(ladder,v)){
-//                System.out.println(String.format("%d 번줄 실패", v));
                 return false;
             }
         }
@@ -107,7 +88,6 @@ public class Ladder{
             ladder[v+1][h]=-1;
             return ladder;
         }
-//        System.out.println(String.format("%d, %d 추가 불가", v,h));
         return null;
     }
     private static int[][] delLine(int[][] ladder, int v, int h){
@@ -127,93 +107,17 @@ public class Ladder{
                 continue;
             }
             if(ladder[i][j]==1){
-//                System.out.println(String.format("[1] %d번 줄의 %d번 가로줄을 타고 내려감", i,j));
                 i++;
                 continue;
             }
             if(ladder[i][j]==-1){
-//                System.out.println(String.format("[2] %d번 줄의 %d번 가로줄을 타고 내려감", i,j));
                 i--;
                 continue;
             }
         }
         if(v!=i){
-//            System.out.println(String.format("사다리결과 : %d 같지않음 %d", v,i));
             return false;
         }
-//        System.out.println(String.format("사다리결과 : %d 같음!!! %d", v,i));
         return true;
-
-    }
-
-    private static int count(int[][] ladder){
-        int cnt =0;
-        for(int i =1;i<=N;i++){
-            for (int j=1;j<=H;j++){
-                if(ladder[i][j]==1){
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-    private static void printLadder(int[][] ladder){
-        for(int i =1 ;i<=N;i++){
-            for(int j=1;j<=H;j++){
-                System.out.printf(String.format("%10d,", ladder[i][j]));
-            }
-            System.out.println();
-        }
-        System.out.println();
     }
 }
-
-/*public class Ladder {
-    static int N, M, H;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-
-        int[][] ladder = new int[N + 1][H + 1];
-        for (int i = 1; i <= M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            // 왼쪽만 있으면 -1, 오른쪽만 있으면 1, 둘다 없으면 0, 둘다 있는 경우는 없다
-            ladder[b][a] = 1; // b번 - b+1번 세로줄 사이의 a 번째 가로줄이 존재
-            if (b + 1 <= N)
-                ladder[b + 1][a] = -1; // b번 - b+1번 세로줄 사이의 a 번째 가로줄이 존재
-        }
-//        printLadder(ladder);
-        boolean[][] visited = new boolean[N+1][H+1];
-        System.out.println(cnt);
-    }
-    private  static void dfs(int[][] ladder, boolean[][] visited){
-        for(int cnt=0;cnt<4;cnt++){
-            Queue<int[]> nodes = available(ladder,visited);
-            while(!nodes.isEmpty()){
-                int[] node = nodes.poll();
-
-            }
-        }
-
-
-    }
-    private static Queue<int[]> available(int[][] ladder,boolean[][] visited){
-        Queue<int[]> nodes = new LinkedList<>();
-        for(int v=1;v<=N;v++){
-            for(int h=1;h<=H;h++){
-                if(!visited[v][h] && ladder[v][h]==0){
-                    int[] node = new int[]{v,h};
-                    nodes.add(node);
-                }
-            }
-        }
-        nodes.add(null); // 한바퀴가 종료됨. 여기까지 넣어도 안되면 cnt 추가하여서 depth2로 가야함.
-        return nodes;
-    }
-}*/
