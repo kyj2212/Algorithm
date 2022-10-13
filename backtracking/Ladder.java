e242f443e352bd4e5b8893690fc6b31bd3a51ea5
kyj2212 <kyj2212@gmail.com>
2022-10-13 14:22:48 +0900

    Feat: all count (brute force) by list cnt, and sout(min.cnt), 1936ms

--- backtracking/Ladder.java
+++ backtracking/Ladder.java
@@ -1,3 +1,4 @@
+
 package backtracking;
 
 import java.io.BufferedReader;
@@ -26,55 +27,73 @@ public class Ladder{
                 ladder[b+1][a]=-1; // b번 - b+1번 세로줄 사이의 a 번째 가로줄이 존재
         }
 //        printLadder(ladder);
-        int cnt = dfs(0,ladder,0);
-        System.out.println(cnt);
+        List<int[]> nodes = new ArrayList<>();
+        List<Integer> cnt = new ArrayList<>();
+
+        if(!isSame(ladder)){
+            cnt = dfs(ladder,0,nodes,cnt);
+        }else{
+            cnt.add(0);
+        }
+        int answer = cnt==null || cnt.isEmpty() ? -1 : Collections.min(cnt);
+        System.out.println(answer);
     }
 
 
-    private static int dfs(int cnt, int[][] ladder,int depth){
+    private static List<Integer> dfs(int[][] ladder,int depth,List<int[]> nodes,List<Integer> cnt){
 
-        if(isSame(ladder)){
-            System.out.println(String.format("성공 : cnt : %d, ",cnt));
-            printLadder(ladder);
+        if(nodes.size()>4){
             return cnt;
         }
+
         for(int v=1;v<=N;v++){
             for(int h=1;h<=H;h++){
+
                 int[][] tmp = addLine(ladder,v,h);
+                int [] node = new int[]{v,h};
                 if(tmp==null){
                     continue;
                 }
                 ladder=tmp;
-                cnt++;
-                System.out.println(String.format("cnt: %d, (%d,%d) 추가",cnt, v,h));
-                System.out.println(String.format("ladder size: %d, cnt: %d", count(ladder)-M,cnt));
+//                cnt++;
+                nodes.add(node);
+//                printNode(nodes);
+//                System.out.println(String.format("cnt: %d, (%d,%d) 추가",cnt, v,h));
+//                System.out.println(String.format("ladder size: %d, cnt: %d", count(ladder)-M,cnt));
                 if(isSame(ladder)){
-                    printLadder(ladder);
-                    return cnt;
+//                    printLadder(ladder);
+                    cnt.add(nodes.size());
                 }
-                if(cnt<3){
-                    depth = dfs(cnt,ladder,depth);
-                    System.out.println(String.format("depth: %d, cnt: %d", depth,cnt));
-                    if(depth!=-1){
-                        System.out.println(String.format("depth: %d가 -1이 아닌경우", depth));
-                        return depth;
-                    }
+                if(nodes.size()<3){
+//                    List<Integer> result = dfs(ladder,depth,nodes,cnt);
+                    cnt = dfs(ladder,depth,nodes,cnt);
+//                    System.out.println(String.format("depth: %d, cnt: %d", depth,cnt));
+//                    if(result!=null){
+////                        System.out.println(String.format("depth: %d가 -1이 아닌경우", depth));
+//                        return result;
+//                    }
 
                 }
                 ladder=delLine(ladder,v,h);
-                cnt--;
-                System.out.println(String.format("cnt: %d, (%d,%d) 제거", cnt,v,h));
-                System.out.println(String.format("ladder size: %d, cnt: %d", count(ladder)-M,cnt));
+                nodes.remove(node);
+//                System.out.println(String.format("cnt: %d, (%d,%d) 제거", cnt,v,h));
+//                System.out.println(String.format("ladder size: %d, cnt: %d", count(ladder)-M,cnt));
             }
         }
-        System.out.println(String.format("cnt : %d,다돌았는데 no same, cnt>=3, cnt -1 반환 ", cnt));
-        return -1;
+//        System.out.println(String.format("cnt : %d,다돌았는데 no same, cnt>=3, cnt -1 반환 ", cnt));
+        return cnt;
+    }
+
+    private static void printNode(List<int[]> nodes) {
+        System.out.printf("nodes : ");
+        nodes.forEach(node -> System.out.printf(String.format("(%d,%d) ", node[0],node[1])));
+        System.out.println();
     }
 
     private static boolean isSame(int[][] ladder){
         for(int v=1;v<=N;v++){
             if(!down(ladder,v)){
-                System.out.println(String.format("%d 번줄 실패", v));
+//                System.out.println(String.format("%d 번줄 실패", v));
                 return false;
             }
         }
@@ -88,6 +107,7 @@ public class Ladder{
             ladder[v+1][h]=-1;
             return ladder;
         }
+//        System.out.println(String.format("%d, %d 추가 불가", v,h));
         return null;
     }
     private static int[][] delLine(int[][] ladder, int v, int h){
@@ -107,21 +127,21 @@ public class Ladder{
                 continue;
             }
             if(ladder[i][j]==1){
-                System.out.println(String.format("[1] %d번 줄의 %d번 가로줄을 타고 내려감", i,j));
+//                System.out.println(String.format("[1] %d번 줄의 %d번 가로줄을 타고 내려감", i,j));
                 i++;
                 continue;
             }
             if(ladder[i][j]==-1){
-                System.out.println(String.format("[2] %d번 줄의 %d번 가로줄을 타고 내려감", i,j));
+//                System.out.println(String.format("[2] %d번 줄의 %d번 가로줄을 타고 내려감", i,j));
                 i--;
                 continue;
             }
         }
         if(v!=i){
-            System.out.println(String.format("사다리결과 : %d 같지않음 %d", v,i));
+//            System.out.println(String.format("사다리결과 : %d 같지않음 %d", v,i));
             return false;
         }
-        System.out.println(String.format("사다리결과 : %d 같음!!! %d", v,i));
+//        System.out.println(String.format("사다리결과 : %d 같음!!! %d", v,i));
         return true;
 
     }
@@ -147,3 +167,53 @@ public class Ladder{
         System.out.println();
     }
 }
+
+/*public class Ladder {
+    static int N, M, H;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        int[][] ladder = new int[N + 1][H + 1];
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            // 왼쪽만 있으면 -1, 오른쪽만 있으면 1, 둘다 없으면 0, 둘다 있는 경우는 없다
+            ladder[b][a] = 1; // b번 - b+1번 세로줄 사이의 a 번째 가로줄이 존재
+            if (b + 1 <= N)
+                ladder[b + 1][a] = -1; // b번 - b+1번 세로줄 사이의 a 번째 가로줄이 존재
+        }
+//        printLadder(ladder);
+        boolean[][] visited = new boolean[N+1][H+1];
+        System.out.println(cnt);
+    }
+    private  static void dfs(int[][] ladder, boolean[][] visited){
+        for(int cnt=0;cnt<4;cnt++){
+            Queue<int[]> nodes = available(ladder,visited);
+            while(!nodes.isEmpty()){
+                int[] node = nodes.poll();
+
+            }
+        }
+
+
+    }
+    private static Queue<int[]> available(int[][] ladder,boolean[][] visited){
+        Queue<int[]> nodes = new LinkedList<>();
+        for(int v=1;v<=N;v++){
+            for(int h=1;h<=H;h++){
+                if(!visited[v][h] && ladder[v][h]==0){
+                    int[] node = new int[]{v,h};
+                    nodes.add(node);
+                }
+            }
+        }
+        nodes.add(null); // 한바퀴가 종료됨. 여기까지 넣어도 안되면 cnt 추가하여서 depth2로 가야함.
+        return nodes;
+    }
+}*/
