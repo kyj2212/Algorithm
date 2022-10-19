294fad25c4f78ff167d23bd44b5c03fb1d70c4f8
kyj2212 <kyj2212@gmail.com>
2022-10-19 18:40:12 +0900

    Feat: if pq.poll()== null ㅊcase add, success

--- heap/Scovile.java
+++ heap/Scovile.java
@@ -6,6 +6,7 @@ import java.util.PriorityQueue;
 public class Scovile {
 
     public static void main(String[] args) {
+//        int[] scoville = {0, 0, 0, 0, 0, 0};
         int[] scoville = {1, 2, 3, 9, 10, 12};
         int K = 7;
 
@@ -18,15 +19,22 @@ public class Scovile {
         }
 
         while(pq.peek()<K){
-            mix(pq,K);
+            if(!mix(pq,K)){
+                answer=-1;
+                break;
+            }
             answer++;
         }
         System.out.println(answer);
     }
 
-    private static void mix(PriorityQueue<Integer> pq,int K){
+    private static boolean mix(PriorityQueue<Integer> pq,int K){
+        if(pq.size()<2){
+            return false;
+        }
         int min1 = pq.poll();
         int min2 = pq.poll();
         pq.add(min1 + min2*2);
+        return true;
     }
 }
