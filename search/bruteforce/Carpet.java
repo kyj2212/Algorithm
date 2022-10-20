515090a8d0e8d90732e14125a15eabfa78ac7929
kyj2212 <kyj2212@gmail.com>
2022-10-21 00:52:35 +0900

    Feat: 1 loop, success

--- search/bruteforce/Carpet.java
+++ search/bruteforce/Carpet.java
@@ -4,31 +4,17 @@ import java.util.Arrays;
 
 public class Carpet {
     public static void main(String[] args) {
-        int brown = 8;
-        int yellow = 1;
+        int brown = 10;
+        int yellow = 2;
 
         int[] answer = {};
 
-        int row=3, column=3;
+        for(int c =3;c<=brown/4 +1;c++){
 
-//        brown = row*2 + (column-2)*2;
-//        yellow = (row-2)*(column-2);
-//
-//        row = brown/2 -column +2;
-//        row = yellow/(column-2) +2;
-//
-
-        L1 :
-        for(int r = 3;r<=brown/2;r++){
-            for(int c = r;c>=1;c--){
-                if(brown == r*2 + (c-2)*2 && yellow == (r-2)*(c-2)){
-                    row=r;
-                    column=c;
-                    break L1;
-                }
+            if((brown*(c-2)) - (2*c*(c-2)) == 2*yellow){
+                System.out.println( brown/2 -c +2+ " "+ c);
             }
         }
-        answer = new int[]{row,column};
-        System.out.println(row + " " + column);
+
     }
 }
