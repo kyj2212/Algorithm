b02b6fd2acefd8122cdf56704f3e579c24524494
kyj2212 <kyj2212@gmail.com>
2022-10-22 18:07:44 +0900

    Feat: use Math.max(), with triangle size, reduce overlapping

--- dp/Triangle.java
+++ dp/Triangle.java
@@ -6,30 +6,19 @@ public class Triangle {
     public static void main(String[] args) {
 
         int[][] triangle = {{7}, {3, 8}, {8, 1, 0},{2, 7, 4, 4}, {4, 5, 2, 6, 5}};
-        int size = triangle.length;
-        int [][] dp = new int[3][(int)Math.pow(2,size-1)];
-        dp[0][0]=triangle[0][0];
-
-        for(int i =0;i<size-1;i++){
-
-            for(int j =0;j<(int)Math.pow(2,i+1);j+=2){
-                int k = dp[2][j/2];
-                dp[1][j]=dp[0][j/2]+triangle[i+1][k];
-                dp[1][j+1]=dp[0][j/2]+triangle[i+1][k+1];
-                dp[2][j]=k;
-                dp[2][j+1]=k+1;
+        int[] dp = new int[triangle.length];
+        int[] prev = new int[triangle.length];
+        dp[0]=triangle[0][0];
+        for(int i=0;i< triangle.length;i++){
+            prev = dp.clone();
+            for(int j =0;j< i;j++){
+                dp[j]=Math.max(dp[j],prev[j]+triangle[i][j]);
+                dp[j+1]=Math.max(dp[j+1],prev[j]+triangle[i][j+1]);
             }
-            for(int j =0;j<(int)Math.pow(2,i);j++){
-                dp[0][j]=dp[1][j];
-            }
-
-//            Arrays.stream(dp[1]).forEach(x -> System.out.printf(x + ","));
-//            System.out.println();
-
+            Arrays.stream(dp).forEach(x -> System.out.printf(x+","));
+            System.out.println();
         }
-
-        System.out.println(Arrays.stream(dp[1]).max().getAsInt());
-
+        System.out.println(Arrays.stream(dp).max().getAsInt());
     }
 
 }
