d9c947d95b4191d959b5179fd72eda380280ab6d
kyj2212 <kyj2212@gmail.com>
2022-10-24 21:14:20 +0900

    Fix: use union(a,b), find(a), but failed

--- search/Network.java
+++ search/Network.java
@@ -3,6 +3,8 @@ package search;
 import java.util.*;
 
 public class Network {
+
+    static int[] nodes;
     public static void main(String[] args) {
 
 //        String str = "[1, 1, 0], [1, 1, 1], [0, 1, 1]";
@@ -15,12 +17,19 @@ public class Network {
         int[][] computers = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
 
         int answer =0;
-        int[] nodes = new int[n];
+        nodes = new int[n];
         for(int i=0;i<n;i++){
             nodes[i]=i;
         }
-        for(int i =0;i<n;i++){
-            union(n,computers,nodes,i);
+//        for(int i =0;i<n;i++){
+//            union(n,computers,nodes,i);
+//        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && computers[i][j]==1){
+                    union(i,j);
+                }
+            }
         }
 
 //        Arrays.stream(nodes).forEach(x -> System.out.printf(x+" "));
@@ -34,17 +43,37 @@ public class Network {
         }
         System.out.println(answer);
     }
-    private static void union(int n, int[][] computers,int[] nodes,int pNode){
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=0;i< n;i++){
-            if(computers[pNode][i]==1 && i!=pNode){
-                queue.add(i);
-            }
+    private static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+
+        if(a!=b){
+            nodes[b]=a;
         }
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            nodes[node]=nodes[pNode];
+
+    }
+    private static int find(int a){
+        if(a ==nodes[a]){
+            return a;
         }
+        return nodes[a]=find(nodes[a]);
     }
 
 }
+//    private static void union(int n, int[][] computers,int[] nodes,int pNode){
+//        Queue<Integer> queue = new LinkedList<>();
+//        for(int i=0;i< n;i++){
+//            if(computers[pNode][i]==1 && i!=pNode){
+//                queue.add(i);
+//            }
+//        }
+//        while(!queue.isEmpty()){
+//            int node = queue.poll();
+//            if(pNode == nodes[pNode]){
+//                nodes[node]=pNode;
+//                return pNode;
+//            }
+//            nodes[node]=union(n,computers,nodes,nodes[pNode]);
+//        }
+//        return nodes[pNode];
+//    }
