0ed7f2f46179a4f4145119de084d9f53b0529a74
kyj2212 <kyj2212@gmail.com>
2022-11-03 22:12:02 +0900

    Feat: use min only width, success

--- search/MinRectangle.java
+++ search/MinRectangle.java
@@ -3,44 +3,40 @@ package search;
 public class MinRectangle {
     static int len;
     public static void main(String[] args) {
-//        int[][] size = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        int[][] size = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
 //        int[][] size = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
-        int[][] size = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};
-        String str = "[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]";
-        str =str.replace('[','{');
-        str =str.replace(']','}');
+//        int[][] size = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};
+//        String str = "[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]";
+//        str =str.replace('[','{');
+//        str =str.replace(']','}');
 //        System.out.println(str);
 
         int answer = 0;
 
-        answer = findMax(0,size,new int[2]);
+
+        answer=findMax(size);
+
         System.out.println(answer);
 
     }
+    private static int findMax(int[][] sizes){
+        // 초기값
+        int max_w=0;
+        int max_h=0;
 
-    private static int findMax(int i , int[][] size, int[] max){
-        if(i==size.length){
-            return max[0]*max[1];
-        }
-        int[] tmp = max.clone();
-        int[] card = size[i];
-        if(card[0]>max[0]){
-            max[0]=card[0];
-        }
-        if(card[1]>max[1]){
-            max[1]=card[1];
-        }
-        int area = findMax(i+1,size,max);
-        max[0]=tmp[0];
-        max[1]=tmp[1];
+        for(int i =0;i<sizes.length;i++){
 
-        if(card[1]>max[0]){
-            max[0]=card[1];
+            int[] node = sizes[i];
+            // 작은 값을 가로
+            if(node[0]<node[1]){
+                max_w=Math.max(max_w,node[0]);
+                max_h=Math.max(max_h,node[1]);
+            }else{
+                max_w=Math.max(max_w,node[1]);
+                max_h=Math.max(max_h,node[0]);
+            }
         }
-        if(card[0]>max[1]){
-            max[1]=card[0];
-        }
-        area = Math.min(area,findMax(i+1,size,max));
-        return area;
+        return max_h*max_w;
     }
+
 }
