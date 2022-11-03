4b59bd6cc662e4e48b5730c96ff1b0fdfd33478f
kyj2212 <kyj2212@gmail.com>
2022-11-03 23:44:13 +0900

    Fix: use bfs, but failed

--- search/Fatigue.java
+++ search/Fatigue.java
@@ -16,10 +16,48 @@ public class Fatigue {
         // 최소 필요 피로도
         // 소모 피로도
         boolean[] visited = new boolean[dungeons.length];
-        int answer =dfs(k,dungeons,0,0);
+//        int answer =dfs(k,dungeons,0,0);
+        int answer = bfs(k,dungeons);
         System.out.println(answer);
     }
 
+    private static int bfs(int k , int[][] dungeons){
+        // 초기화
+        int cnt =0;
+        int[][] visited = new int[dungeons.length][dungeons.length];
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i =0;i<dungeons.length;i++){
+            queue.add(new int[]{k,cnt,i});
+        }
+        while(!queue.isEmpty()){
+
+            int[] node = queue.poll();
+            cnt=node[1];
+            visited[cnt][node[2]]=1;
+            int[] tmp = explore(dungeons,node);
+            if(tmp[1]==dungeons.length){
+                return tmp[1];
+            }
+            for(int i =0;i<dungeons.length;i++){ // visited[0]=0, visited[4]=1 이야 4번노드는 1이지만 3번노드가 들린게 아니야, 근데 자기와 같은 depth 인데 visited 이면 앞의 큐에서 계산햇을거야
+                if(visited[tmp[1]][i]==1){
+                    continue;
+                }
+                queue.add(new int[]{tmp[0],tmp[1],i});
+            }
+        }
+        return cnt;
+    }
+    private static int[] explore(int[][] dungeons,int[] node){
+        int k=node[0];
+        int cnt=node[1];
+        int[] dungeon = dungeons[node[2]];
+        if(dungeon[0]<=k){
+            k-=dungeon[1];
+            cnt++;
+        }
+        return new int[]{k,cnt};
+    }
+/*
     private static int dfs(int k, int[][]dungeons, int i,int cnt){
         if(i==dungeons.length){
             System.out.println(String.format("cnt : %d,  k : %d", cnt,k));
@@ -36,7 +74,7 @@ public class Fatigue {
         }
 
        return Math.max(tmpCnt,dfs(tmpK,dungeons,i+1,cnt));
-    }
+    }*/
 
 
 }
