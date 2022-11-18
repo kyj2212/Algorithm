533591dfebeb547e8ee98ec149945666dd42163e
kyj2212 <kyj2212@gmail.com>
2022-11-18 18:11:10 +0900

    Feat: use dfs and dp

--- simulation/lunchtime/Solution.java
+++ simulation/lunchtime/Solution.java
@@ -12,9 +12,6 @@ public class Solution {
 	public static void main (String args[]) throws IOException {
 		
 		
-		//Scanner sc = new Scanner(System.in);
-		//int T;
-		//T=sc.nextInt();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T  = Integer.parseInt(br.readLine());
 		
@@ -47,12 +44,8 @@ public class Solution {
 				}
 				
 			}
-//			Arrays.stream(sl[0]).forEach(i -> System.out.printf(i+" "));
-//			System.out.println();
-//			Arrays.stream(sl[1]).forEach(i -> System.out.printf(i+" "));
-//			System.out.println();
-			int[][] p = new int[pl.size()][2];
 
+			int[][] p = new int[pl.size()][2];
 			System.out.println("#"+test_case+" "+findMin(N,0,pl,p,sl));
 		}
 		
@@ -69,33 +62,16 @@ public class Solution {
 		int s2 = Math.abs(pl.get(i)[0]-sl[1][0])+Math.abs(pl.get(i)[1]-sl[1][1]);
 		
 		
-			
-//			System.out.println("i "+i+" s1 : "+s1+" s2 : "+s2);
-		
+
 			p[i][0]=s1;
 			p[i][1]=1;
-			
 			answer = Math.min(answer,findMin(N,i+1,pl,p,sl));
 			
 			
 			p[i][0]=s2;
 			p[i][1]=2;
-			
 			answer = Math.min(answer,findMin(N,i+1,pl,p,sl));
 			return answer;
-//		for(int j =0;j<p.length;j++) {
-//			s1 = Math.abs(pl.get(j)[0]-sl[0][0])+Math.abs(pl.get(j)[1]-sl[0][1]);
-//			s2 = Math.abs(pl.get(j)[0]-sl[1][0])+Math.abs(pl.get(j)[1]-sl[1][1]);
-//			if(j==2 ||j==5 ||j==6) {
-//				p[j][0]=s1;
-//				p[j][1]=1;
-//			}else {
-//				p[j][0]=s2;
-//				p[j][1]=2;
-//			}
-//		}
-//		return getTime(N,p,sl);
-		
 
 	}
 	
@@ -105,21 +81,6 @@ public class Solution {
 		int maxTime=N*2+11;
 		int[][] time = new int[p.length][maxTime];
 
-//		for(int j=0;j<p.length;j++) {
-//			System.out.printf(p[j][1]+" ");
-//		}
-//		System.out.println();
-//
-//		for(int j=0;j<p.length;j++) {
-//			System.out.printf(p[j][0]+" ");
-//		}
-//		System.out.println();
-
-//	for(int i =1;i<maxTime;i++) {
-//		for(int j=0;j<pl.size();j++) {
-//			time[j][i]=-1;
-//		}
-//	}
 		for(int j=0;j<p.length;j++) {
 			time[j][0]=p[j][0];
 		}
@@ -128,16 +89,6 @@ public class Solution {
 
 			for(int i =1;i<maxTime;i++) {
 				int cnt =0;
-//				int[] s = new int[2];
-//				for(int j =0;j<pl.size();j++) {
-//					if(isStair[j]==1) {
-//						s[0]++;
-//					}
-//					if(isStair[j]==2) {
-//						s[1]++;
-//					}
-//				}
-//				System.out.println("1번 입구 "+s[0]+" 2번입구 "+s[1]);
 				for(int j=0;j<p.length;j++) {
 
 					if(time[j][i-1]>0) {
@@ -149,21 +100,7 @@ public class Solution {
 					}
 					
 					if(cnt>=p.length) {
-//						for(int k=0;k<pl.size();k++) {
-//							System.out.printf(isStair[k]+" ");
-//						}
-//						System.out.println();
-						
-//						for(int k=0;k<p.length;k++) {
-//							Arrays.stream(time[k]).forEach(x -> System.out.printf("%2d ",x));
-//							System.out.println();
-//						}
-//						System.out.println("time : "+(i));
-//							
-						
 						return i;
-//						System.out.println("#"+T+" "+(i-1));
-//						break L1;
 					}
 					
 				}
@@ -171,23 +108,18 @@ public class Solution {
 				for(int j =0;j<p.length;j++) {
 					if(time[j][i-1]==0 && isStair[j]==0) {
 						int ent = p[j][1];
-//						System.out.println(j+"번사람 : "+s[ent-1]+" 이라서 못들어가");
 						if(cnt(ent,isStair)<3) {
 							time[j][i]=sl[ent-1][2];
 							isStair[j]=ent;
-//							s[ent-1]++;
 							
 						}else {
 							time[j][i]=0;
 						}
 						continue;
 					}
-					
 				}
-				
 			}
 
-		
 		return maxTime;
 	}
 	
@@ -198,7 +130,6 @@ public class Solution {
 				cnt++;
 			}
 		}
-//		System.out.println(ent+"번 계단 사용자 : "+cnt);
 		return cnt;
 	}
 	
